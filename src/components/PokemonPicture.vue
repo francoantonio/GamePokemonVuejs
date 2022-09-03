c11d72141979c37b35998bd92199ad554e5a05d0
Antonio Franco <antoniofranco.dev@gmail.com>
2022-09-03 17:55:13 -0300

    first commit

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="pokemon-container">
+    <img
+      :src="imgSrc"
+      v-bind:class="{ 'fade-in': showPokemon, 'pokemon-hidden': !showPokemon }"
+      alt="pokemon"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonPicture",
+
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return this.pokemonId
+        ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+        : "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pokemon-hidden {
+  filter: brightness(0);
+}
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>¿Quién es ese Pokémon?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+    <PokemonOption :pokemons="pokemonArr" @selection="checkAnswer" />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo juego</button>
+    </template>
+  </div>
+</template>
+
+<script>
+import PokemonOption from "@/components/PokemonOption.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+export default {
+  name: "PokemonPage",
+  components: { PokemonPicture, PokemonOption },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: {},
+      showPokemon: false,
+      shoAnswer: false,
+      message: "",
+    };
+  },
+  methods: {
+    newGame() {
+      (this.pokemonArr = []),
+        (this.pokemon = {}),
+        (this.showPokemon = false),
+        (this.shoAnswer = false),
+        (this.message = ""),
+        this.mixPokemonArray();
+    },
+    checkAnswer(pokemonId) {
+      if (this.showPokemon) return;
+      this.showPokemon = true;
+      this.message =
+        pokemonId == this.pokemon.id
+          ? `Ganaste, ${this.pokemon.name}`
+          : `Oops, era ${this.pokemon.name}`;
+      this.showAnswer = true;
+    },
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions(this.pokemonArr);
+
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
